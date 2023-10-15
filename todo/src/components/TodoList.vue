3fe9beec5798988c7bd33de1fb2153cc2359d7b7
yeswon9874 <yeswon9874@gmail.com>
2023-10-15 15:57:01 +0900

    2023/10/15 Vuex 생성

---
+++ todo/src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <TodoComponent 
+            v-for="todo in todos" 
+            :key="todo.id"
+            :todo="todo"
+            @toggle-checkbox="toggleCheckbox"
+            @click-delete="deleteTodo"
+        />
+    </div>
+</template>
+
+<script>
+import TodoComponent from '@/components/TodoComponent.vue'
+export default {
+    components: {
+        TodoComponent
+    },
+    props: {
+        todo: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        toggleCheckbox(value) {
+            this.$emit('toggle-checkbox', value)
+        },
+        deleteTodo(todoId) {
+            this.$emit('click-delete', todoId);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
