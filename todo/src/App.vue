e2ea6a3b8f30286719cf57ca4fba6fd7105e2703
yeswon9874 <yeswon9874@gmail.com>
2023-10-15 14:58:24 +0900

    2023/10/15 todo 앱 생성2

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -9,7 +9,10 @@
     <hr>
     <TodoComponent v-for="todo in todos" 
     :key="todo.id"
-    :todo="todo"/>
+    :todo="todo"
+    @toggle-checkbox="toggleCheckbox"
+    @click-delete="deleteTodo"
+    />
   </div>
 </template>
 
@@ -29,6 +32,14 @@ export default {
   }
  },
  methods: {
+  deleteTodo(id) {
+    // const index = this.todos.findIndex(todo => {
+    //   return todo.id === id;
+    // });
+    // this.todos.splice(index, 1);
+
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  },
   addTodo(e) {
     this.todos.push({
       id: Math.random(),
@@ -36,6 +47,12 @@ export default {
       checked: false
     });
     this.todoText ='';
+  },
+  toggleCheckbox({id, checked}) {
+    const index = this.todos.findIndex(todo => {
+      return todo.id === id;
+    })
+    this.todos[index].checked = checked;
   }
  }
 }
