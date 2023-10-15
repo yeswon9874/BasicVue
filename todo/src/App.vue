b70b56cf7d56139f22bbbee2af2eb00526858330
yeswon9874 <yeswon9874@gmail.com>
2023-10-15 14:33:03 +0900

    2023/10/15 todo앱 생성

---
+++ todo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+      v-model="todoText"
+      type="text" class="w-100 p-2" 
+      placeholder="Type todo"
+      @keyup.enter="addTodo">
+    <hr>
+    <TodoComponent v-for="todo in todos" 
+    :key="todo.id"
+    :todo="todo"/>
+  </div>
+</template>
+
+<script>
+import TodoComponent from './components/TodoComponent.vue';
+export default {
+ components: {
+  TodoComponent
+ },
+ data() {
+  return {
+    todoText: '',
+    todos: [
+      {id: 1, text: 'buy a car', checked: false},
+      {id: 2, text: 'play game', checked: false},
+    ]
+  }
+ },
+ methods: {
+  addTodo(e) {
+    this.todos.push({
+      id: Math.random(),
+      text: e.target.value,
+      checked: false
+    });
+    this.todoText ='';
+  }
+ }
+}
+</script>
