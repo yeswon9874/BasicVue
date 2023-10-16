98201038886febaebb366755792354fde83dec9c
yeswon9874 <yeswon9874@gmail.com>
2023-10-16 23:41:17 +0900

    2023/10/16 Vuex Actions

--- todo23/src/store/index.js
+++ todo23/src/store/index.js
@@ -3,14 +3,19 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+import axios from 'axios';
 export default new Vuex.Store({
     state: {
         todos: [
             { id: 1, text: 'buy a car', checked: false},
             { id: 2, text: 'play game', checked: false},
-        ]
+        ],
+        users: []
     },
     mutations: {
+        SET_USERS(state, users) {
+            state.users = users;
+        },
         ADD_TODO(state, value) {
             state.todos.push({
                 id: Math.random(),
@@ -32,7 +37,26 @@ export default new Vuex.Store({
         }
     },
     actions: {
-
+        getUsers({commit}) {
+            axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+                commit('SET_USERS', res.data);
+            });
+        },
+        addTodo({commit}, value) {
+            setTimeout(() => {
+                commit('ADD_TODO', value);
+            }, 500);
+        },
+        toggleTodo({commit}, payload) {
+            setTimeout(() => {
+                commit('TOGGLE_TODO', payload);
+            }, 500);
+        },
+        delteTodo({commit}, todoId) {
+            setTimeout(() => {
+                commit('DELETE_TODO', todoId);
+            }, 500);
+        },
     },
     getters: {
 
